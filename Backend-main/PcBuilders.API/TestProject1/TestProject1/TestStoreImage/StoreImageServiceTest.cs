f486ef741f8e323c4db8d1dae061c5c8cc5d53db
ELÍAS TORRES ESPINOZA <102787891+EliasTorresEzpinoza@users.noreply.github.com>
2023-10-29 19:15:21 -0500

    Update StoreImageServiceTest.cs

--- Backend-main/PcBuilders.API/TestProject1/TestProject1/TestStoreImage/StoreImageServiceTest.cs
+++ Backend-main/PcBuilders.API/TestProject1/TestProject1/TestStoreImage/StoreImageServiceTest.cs
@@ -2,7 +2,7 @@ using System;
 using Xunit;
 namespace TestProject1.TestStoreImage
 {
-    public class StoreImageServiceTest
+    public class StoreImageServiceClasTests
     {
         [Fact]
         public void StoreImageServiceClas_SetProperties_ReturnsExpectedResult()
@@ -11,7 +11,7 @@ namespace TestProject1.TestStoreImage
             var storeImageServiceClas = new StoreImageServiceClas();
             int expectedId = 1;
             string expectedEnconded64Image = "Test Enconded64Image";
-            int expectedStoreId = 2;
+            int expectedStoreId = 1;
 
             // Act
             storeImageServiceClas.Id = expectedId;
@@ -22,6 +22,36 @@ namespace TestProject1.TestStoreImage
             Assert.Equal(expectedId, storeImageServiceClas.Id);
             Assert.Equal(expectedEnconded64Image, storeImageServiceClas.Enconded64Image);
             Assert.Equal(expectedStoreId, storeImageServiceClas.StoreId);
+        }
+
+        [Fact]
+        public void StoreImageServiceClas_SetInvalidId_ThrowsException()
+        {
+            // Arrange
+            var storeImageServiceClas = new StoreImageServiceClas();
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => storeImageServiceClas.Id = -1);
+        }
+
+        [Fact]
+        public void StoreImageServiceClas_SetEmptyEnconded64Image_ThrowsException()
+        {
+            // Arrange
+            var storeImageServiceClas = new StoreImageServiceClas();
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => storeImageServiceClas.Enconded64Image = string.Empty);
+        }
+
+        [Fact]
+        public void StoreImageServiceClas_SetInvalidStoreId_ThrowsException()
+        {
+            // Arrange
+            var storeImageServiceClas = new StoreImageServiceClas();
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => storeImageServiceClas.StoreId = -1);
         }
     }
 }
