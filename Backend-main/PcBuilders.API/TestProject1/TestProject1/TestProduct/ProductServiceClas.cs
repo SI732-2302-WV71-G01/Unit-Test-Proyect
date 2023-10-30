922e64e86f22c1b265550668dc0617d297a18d59
ELÍAS TORRES ESPINOZA <102787891+EliasTorresEzpinoza@users.noreply.github.com>
2023-10-29 19:02:58 -0500

    Update ProductServiceClas.cs

--- Backend-main/PcBuilders.API/TestProject1/TestProject1/TestProduct/ProductServiceClas.cs
+++ Backend-main/PcBuilders.API/TestProject1/TestProject1/TestProduct/ProductServiceClas.cs
@@ -1,18 +1,120 @@
-
+using System;
 namespace TestProject1.TestProduct
 {
-    public class ProductServiceClas
+     public class ProductServiceClas
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Rating { get; set; }
-        public string Image { get; set; }
-        public string Category { get; set; }
-        public int Price { get; set; }
-        public string InventoryStatus { get; set; }
-        public int StoreId { get; set; }
-        //public Store Store { get; set; }
-        //public IList<Purchase> Purchases { get; set; } = new List<Purchase>();
-        //public IList<Sale> Sales { get; set; } = new List<Sale>();
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Id cannot be negative");
+                }
+                _id = value;
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty");
+                }
+                _name = value;
+            }
+        }
+
+        private int _rating;
+        public int Rating
+        {
+            get { return _rating; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Rating cannot be negative");
+                }
+                _rating = value;
+            }
+        }
+
+        private string _image;
+        public string Image
+        {
+            get { return _image; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Image cannot be null or empty");
+                }
+                _image = value;
+            }
+        }
+
+        private string _category;
+        public string Category
+        {
+            get { return _category; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Category cannot be null or empty");
+                }
+                _category = value;
+            }
+        }
+
+        private int _price;
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price cannot be negative");
+                }
+                _price = value;
+            }
+        }
+
+        private string _inventoryStatus;
+        public string InventoryStatus
+        {
+            get { return _inventoryStatus; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("InventoryStatus cannot be null or empty");
+                }
+                _inventoryStatus = value;
+            }
+        }
+
+        private int _storeId;
+        public int StoreId
+        {
+            get { return _storeId; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("StoreId cannot be negative");
+                }
+                _storeId = value;
+            }
+        }
+
+
     }
 }
