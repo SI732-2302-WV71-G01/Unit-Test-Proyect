5152248e033fb92a630d06aba871250989d16e4d
ELÍAS TORRES ESPINOZA <102787891+EliasTorresEzpinoza@users.noreply.github.com>
2023-10-29 19:07:36 -0500

    Update PurchaseServiceTest.cs

--- Backend-main/PcBuilders.API/TestProject1/TestProject1/TestPurchase/PurchaseServiceTest.cs
+++ Backend-main/PcBuilders.API/TestProject1/TestProject1/TestPurchase/PurchaseServiceTest.cs
@@ -2,7 +2,7 @@ using System;
 using Xunit;
 namespace TestProject1.TestPurchase
 {
-    public class PurchaseServiceTest
+    public class PurchaseServiceClasTests
     {
         [Fact]
         public void PurchaseServiceClas_SetProperties_ReturnsExpectedResult()
@@ -22,6 +22,36 @@ namespace TestProject1.TestPurchase
             Assert.Equal(expectedId, purchaseServiceClas.Id);
             Assert.Equal(expectedCode, purchaseServiceClas.Code);
             Assert.Equal(expectedUserId, purchaseServiceClas.UserId);
+        }
+
+        [Fact]
+        public void PurchaseServiceClas_SetInvalidId_ThrowsException()
+        {
+            // Arrange
+            var purchaseServiceClas = new PurchaseServiceClas();
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => purchaseServiceClas.Id = -1);
+        }
+
+        [Fact]
+        public void PurchaseServiceClas_SetEmptyCode_ThrowsException()
+        {
+            // Arrange
+            var purchaseServiceClas = new PurchaseServiceClas();
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => purchaseServiceClas.Code = string.Empty);
+        }
+
+        [Fact]
+        public void PurchaseServiceClas_SetInvalidUserId_ThrowsException()
+        {
+            // Arrange
+            var purchaseServiceClas = new PurchaseServiceClas();
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => purchaseServiceClas.UserId = -1);
         }
     }
 }
