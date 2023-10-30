dfb77bffdb2a12b726446d726198bef32fe0ee6c
ELÍAS TORRES ESPINOZA <102787891+EliasTorresEzpinoza@users.noreply.github.com>
2023-10-29 19:06:50 -0500

    Update PurchaseServiceClas.cs

--- Backend-main/PcBuilders.API/TestProject1/TestProject1/TestPurchase/PurchaseServiceClas.cs
+++ Backend-main/PcBuilders.API/TestProject1/TestProject1/TestPurchase/PurchaseServiceClas.cs
@@ -1,12 +1,50 @@
+using System;
 namespace TestProject1.TestPurchase
 {
     public class PurchaseServiceClas
     {
-        public int Id { get; set; }
-        public string Code { get; set; }
-        public int UserId { get; set; }
-        //Relationship
-        //public User User { get; set; }
-        //public IList<Product> Products { get; set; } = new List<Product>();
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Id cannot be negative");
+                }
+                _id = value;
+            }
+        }
+
+        private string _code;
+        public string Code
+        {
+            get { return _code; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Code cannot be null or empty");
+                }
+                _code = value;
+            }
+        }
+
+        private int _userId;
+        public int UserId
+        {
+            get { return _userId; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("UserId cannot be negative");
+                }
+                _userId = value;
+            }
+        }
+
+       
     }
 }
