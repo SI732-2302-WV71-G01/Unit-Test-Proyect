3b380ac1147747d50c168c0f5175d673d94633dd
ELÍAS TORRES ESPINOZA <102787891+EliasTorresEzpinoza@users.noreply.github.com>
2023-10-29 19:11:46 -0500

    Update StoreServiceClas.cs

--- Backend-main/PcBuilders.API/TestProject1/TestProject1/TestStore/StoreServiceClas.cs
+++ Backend-main/PcBuilders.API/TestProject1/TestProject1/TestStore/StoreServiceClas.cs
@@ -1,16 +1,90 @@
+using System;
 namespace TestProject1.TestStore
 {
     public class StoreServiceClas
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-        //public User User { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Address { get; set; }
-        public string Encoded64LogoImage { get; set; }
-        // public IList<StoreImage> StoreImages { get; set; } = new List<StoreImage>(); 
-        //public IList<Product> Products { get; set; } = new List<Product>(); 
-        //public IList<Sale> Sales { get; set; } = new List<Sale>(); 
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Id cannot be negative");
+                }
+                _id = value;
+            }
+        }
+
+        private int _userId;
+        public int UserId
+        {
+            get { return _userId; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("UserId cannot be negative");
+                }
+                _userId = value;
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty");
+                }
+                _name = value;
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Description cannot be null or empty");
+                }
+                _description = value;
+            }
+        }
+
+        private string _address;
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Address cannot be null or empty");
+                }
+                _address = value;
+            }
+        }
+
+        private string _encoded64LogoImage;
+        public string Encoded64LogoImage
+        {
+            get { return _encoded64LogoImage; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Encoded64LogoImage cannot be null or empty");
+                }
+                _encoded64LogoImage = value;
+            }
+        }
     }
 }
