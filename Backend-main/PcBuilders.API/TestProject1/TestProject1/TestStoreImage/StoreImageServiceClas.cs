a32c2759a62fb67b043dc1d46ee785de6f5779a7
ELÍAS TORRES ESPINOZA <102787891+EliasTorresEzpinoza@users.noreply.github.com>
2023-10-29 19:14:38 -0500

    Update StoreImageServiceClas.cs

--- Backend-main/PcBuilders.API/TestProject1/TestProject1/TestStoreImage/StoreImageServiceClas.cs
+++ Backend-main/PcBuilders.API/TestProject1/TestProject1/TestStoreImage/StoreImageServiceClas.cs
@@ -1,10 +1,49 @@
+using System;
 namespace TestProject1.TestStoreImage
 {
     public class StoreImageServiceClas
     {
-        public int Id { get; set; }
-        public string Enconded64Image { get; set; }
-        public int StoreId { get; set; }
-        //public Store Store { get; set; }
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Id cannot be negative");
+                }
+                _id = value;
+            }
+        }
+
+        private string _enconded64Image;
+        public string Enconded64Image
+        {
+            get { return _enconded64Image; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Enconded64Image cannot be null or empty");
+                }
+                _enconded64Image = value;
+            }
+        }
+
+        private int _storeId;
+        public int StoreId
+        {
+            get { return _storeId; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("StoreId cannot be negative");
+                }
+                _storeId = value;
+            }
+        }
+        
     }
 }
